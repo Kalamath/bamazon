a71e3781efe77d965a76cafcb16b031d28d74774
Kalamath <thecodedimage@gmail.com>
2019-09-13 16:46:52 -0700

    finally did it!

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var Table = require('cli-table');
+// var Table = require('cli-table');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -14,101 +14,112 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-connection.connect();
-
-connection.query('SELECT * FROM products', function (error, results, fields) {
-    if (error) throw error;
-    productView();
-    // console.log("\n ============================================================================");
-    // console.log("\n Welcome to Bamazon")
-    // console.log("\n Check out our HOTTEST items! \n")
-    // console.log(results[0].item_id, results[0].product_name, results[0].price);
-    // console.log(results[1].item_id, results[1].product_name, results[1].price);
-    // console.log(results[2].item_id, results[2].product_name, results[2].price);
-    // console.log(results[3].item_id, results[3].product_name, results[3].price);
-    // console.log(results[4].item_id, results[4].product_name, results[4].price);
-    // console.log(results[5].item_id, results[5].product_name, results[5].price);
-    // console.log(results[6].item_id, results[6].product_name, results[6].price);
-    // console.log(results[7].item_id, results[7].product_name, results[7].price);
-    // console.log(results[8].item_id, results[8].product_name, results[8].price);
-    // console.log(results[9].item_id, results[9].product_name, results[9].price);
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("\n ============================================================================");
+    console.log("\n Welcome to Bamazon")
+    console.log("\n Check out our HOTTEST items! \n")
+    showItems();
 });
 
-connection.end();
-
-// var item;
-
-function productView() {
-    connection.query(
-        'SELECT * FROM products', function (error, results) {
-            // if (error) throw error;
-            var table = new Table({ head: ["Item ID", "Product Name", "Price"] });
-            console.log("\n ============================================================================");
-            console.log("\n Welcome to Bamazon")
-            console.log("\n Check out our latest items!")
-            table.push(
-                ['1', 'Beats Wireless Buds', '$179.99'],
-                ['2', 'PS4', '$299.99'],
-                ['3', 'Nintendo Switch', '$249.99'],
-                ['4', 'Q-Tips', '$4.99'],
-                ['5', 'Shampoo', '$6.99'],
-                ['6', 'Case of Water', '$5.99'],
-                ['7', 'Red Grapes', '$5.99'],
-                ['8', 'Air Jordan 1', '$119.99'],
-                ['9', 'Addidas Tiro Pants', '$34.99'],
-                ['10', 'Panasonic Stereo', '$99.99'],
-            )
-
-            console.log("\n" + table.toString() + "\n");
-
-            buyItem();
+function showItems() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + "$" + res[i].price);
         }
-    )
+        console.log("-----------------------------------");
+        buyItem();
+    })
 }
 
 function buyItem() {
     inquirer
         .prompt({
             name: "toBuy",
-            type: "confirm",
-            message: "Would you like to purchase an item? (Hit Enter for YES)",
+            type: "list",
+            message: "Would you like to purchase an item?",
+            choices: ["BUY", "NO, I'M GOOD"]
         })
         .then(answers => {
-            console.log("\n Awesome!")
-
-            whichItem();
+            if (answers.toBuy === "BUY") {
+                whichItem();
+                console.log("\n Shweet :) \n");
+            } else {
+                connection.end();
+                console.log("\n That's too bad :(");
+            }
         })
 }
 
 function whichItem() {
-    inquirer
-        .prompt({
-            type: 'expand',
-            name: 'theme',
-            message: 'Which Product Would You Like to Buy?',
-            choices: [
-               { 
-                   key : '1',
-                   name : 'Beats Wireless Buds',
-                   value: 'results[0].item_id, results[0].product_name, results[0].price',
-                },
-               { 
-                   key : '2',
-                   name : 'PS4',
-                   value: 'results[1].item_id, results[1].product_name, results[1].price',
-                },
-               { 
-                   key : '3',
-                   name : 'Nintendo Switch',
-                   value: 'results[2].item_id, results[2].product_name, results[2].price',
-                },
-                
-            ]
-        })
-        .then(answers => {
-            console.log(answers);
-        })
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                name: "idChoice",
+                type: "input",
+                message: "What is the ID of the item you'd like?",
+            },
+            {
+                name: "amountChoice",
+                type: "input",
+                message: "How many of this item would you like?"
+            }
+        ])
+            .then((answers) => {
+                // console.log(answers);
+                var itemID = answers.idChoice;
+                var amount = parseInt(answers.amountChoice);
+                // console.log(itemID, amount)
+
+                connection.query(
+                    "SELECT * FROM products WHERE item_id = " + itemID,
+                    function (err, res) {
+                        if (err) throw err;
+
+                        for (var result of res) {
+                            // console.log(result);
+                            var quantity = parseInt(result.stock_quantity);
+                            var cost = parseFloat(result.price);
+                            var totalCost = (cost * amount).toFixed(2);
+                            var newInventory = quantity - amount;
+
+                            console.log("\n Your total will be " + "$" + totalCost + "\n");
+                            inquirer
+                                .prompt({
+                                    name: "checkout",
+                                    type: "confirm",
+                                    message: "Would you like to check out? \n"
+                                })
+                                .then((answers) => {
+                                    if (answers.checkout) {
+                                        inventoryUpdate(newInventory, itemID);
+                                    }
+                                })
+                            // console.log(newInventory);
+                        }
+                    }
+                )
+                // connection.end();
+            })
+    })
 }
+
+function inventoryUpdate(newInventory, itemID) {
+    connection.query(
+        "UPDATE products SET ? WHERE item_id = " + itemID,
+        { stock_quantity: newInventory },
+        function (err, res) {
+            if (err) throw err;
+        });
+    connection.end();
+}
+
+
+
+
+
 
 
 
